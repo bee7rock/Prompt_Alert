20baabd3f242736216ac433d98122c808e12de53
bee7rock <1261@iai.pt>
2025-12-02 17:12:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 let cores = ['Vermelho', 'Verde', 'Amarelo', 'Azul', 'Roxo', 'Rosa', 'Laranja', 'Branco', 'Preto', 'Cinzento', 'Castanho'];
 let cor = cores[Math.floor(Math.random() * cores.length)];
 
-console.log(cor)
-
 let adivinhar = prompt('Adivinha a cor em que estou a pensar:')
-adivinhar = Math.floor(Number(adivinhar))
+adivinhar = Math.floor(adivinhar)
+
+console.log('Cor escolhida: ' + cor, 'Cor visitante: ' + adivinhar)
+
+let ganhou = false;
+
+if (adivinhar != cor) {
+    alert('É necessário inserir uma cor válida, recomeça.')
+} else {
+    while (!ganhou) {
+        if (adivinhar = cor) {
+            alert('Acertaste!!!')
+
+            ganhou = true;
+        }
+    }
+}
